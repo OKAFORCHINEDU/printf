6a2a0767970071957c443df643537db14aa38ab2
OKAFORCHINEDU <chineducornelius.okafor@yahoo.com>
2022-08-09 18:48:30 -0700

    Updated function

--- functions2.c
+++ functions2.c
@@ -22,6 +22,7 @@ int print_pointer(va_list types, char buffer[],
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);
+
 	UNUSED(width);
 	UNUSED(size);
 
@@ -33,15 +34,20 @@ int print_pointer(va_list types, char buffer[],
 
 	while (num_addrs > 0)
 	{
-		buffer[ind--] = map_to[num_addrs % 16];						num_addrs /= 16;
-		length++;									}
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
 	else if (flags & F_SPACE)
-		extra_c = ' ', length++;							ind++;
-	return (write_pointer(buffer, ind, length,width, flags, padd, extra_c, padd_start));}
+		extra_c = ' ', length++;
+	ind++;
+	return (write_pointer(buffer, ind, length, width,
+				flags, padd, extra_c, padd_start));
+}
 
 
 /************************* PRINT NON PRINTABLE *************************/
@@ -65,6 +71,7 @@ int print_non_printable(va_list types, char buffer[],
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
+
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
@@ -109,10 +116,12 @@ int print_reverse(va_list types, char buffer[],
 {
 	char *str;
 	int i, count = 0;
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(size);
+
 	str = va_arg(types, char *);
 
 	if (str == NULL)
@@ -126,6 +135,7 @@ int print_reverse(va_list types, char buffer[],
 	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
+
 		write(1, &z, 1);
 		count++;
 	}
@@ -159,6 +169,7 @@ int print_rot13string(va_list types, char buffer[],
 	int count = 0;
 	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	str = va_arg(types, char *);
 	UNUSED(buffer);
 	UNUSED(flags);
